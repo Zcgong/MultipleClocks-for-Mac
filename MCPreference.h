6b9ab9dfe04d6114a3f547527f1e1de433f179e7
tonyzhou <zhouhb.mac@gmail.com>
2013-05-10 22:57:10 +0800

    implement some preference features

---
+++ MCPreference.h
@@ -0,0 +1,13 @@
+//
+//  MCPreference.h
+//  MultipleClocks
+//
+//  Created by tonyzhou on 5/10/13.
+//  Copyright (c) 2013 tonyzhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCPreference : NSObject
+
+@end
---
+++ MCPreference.m
@@ -0,0 +1,13 @@
+//
+//  MCPreference.m
+//  MultipleClocks
+//
+//  Created by tonyzhou on 5/10/13.
+//  Copyright (c) 2013 tonyzhou. All rights reserved.
+//
+
+#import "MCPreference.h"
+
+@implementation MCPreference
+
+@end
